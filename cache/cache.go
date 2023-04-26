8245bfee468b27912a569d02460b84285805821a
Kozakov <kozakov@lab80.io>
2023-04-26 12:53:17 +0300

    init

--- cache/cache.go
+++ cache/cache.go
@@ -3,12 +3,14 @@ package cache
 import (
 	"fmt"
 	"time"
+    "context"
+    "encoding/json"
 )
 
 type ICache interface {
-	Put(key string, val interface{}) error
-	Get(key string) interface{}
-	Delete(key string) error
+	Put(ctx context.Context, key string, val interface{}) error
+	Get(ctx context.Context, key string) interface{}
+	Delete(ctx context.Context, key string) error
 }
 
 type AbstractCache struct {
@@ -19,52 +21,40 @@ func NewAbstractCache(core ICache) *AbstractCache {
 	return AbstractCache{core: core}
 }
 
-func (a *AbstractCache) ()  {
-	
-}
+type AbstractProc func(ctx context.Context, params ...interface{}) (interface{}, error)
 
-
-func (c *CacheManager) getFromCache(
+func (c *AbstractCache) GetFromCache(
     ctx context.Context,
- keyPrefix string,
- getter func(ctx context.Context, params ...interface{}) (interface{}, error),
- params ...interface{},
+    keyPrefix string,
+    proc AbstractProc,
+    params ...interface{},
 ) ([]byte, error) {
- var (
-  err  error
-  got  interface{}
-  data []byte
- )
+    var (
+        err  error
+        got  interface{}
+        data []byte
+    )
 
- key := keyPrefix
- for _, p := range params {
-  key += fmt.Sprintf("-%v", p)
- }
+    key := keyPrefix
+    for _, p := range params {
+        key += fmt.Sprintf("-%v", p)
+    }
 
- data, err = c.cache.GetData(ctx, key)
- if err != nil {
-  logger.WarnKV(ctx, "error when getting from cache", "error", err)
-  if len(params) > 0 {
-   got, err = getter(ctx, params...)
-  } else {
-   got, err = getter(ctx)
-  }
-  if err != nil {
-   return nil, err
-  }
-  data, _ = json.Marshal(got)
-  key := key
-  got := got
-  go func() {
-   traceCtx := ctxutil.WithValues(context.Background(), ctx)
-   setCtx, setCnl := context.WithTimeout(traceCtx, 20*time.Millisecond)
-   defer setCnl()
-   if putErr := c.cache.Put(setCtx, key, got); putErr != nil {
-    logger.WarnKV(traceCtx, "error when putting in cache", "error", putErr)
-   }
-  }()
- }
+    data, err = c.core.Get(ctx, key)
+    if err != nil {
+        got, err = proc(ctx, params...)
+        if err != nil {
+            return nil, fmt.Errorf("error on proc: %w", err)
+        }
+        
+        data, err = json.Marshal(got)
+        if err != nil {
+            return nil, fmt.Errorf("error on marshal: %w", err)
+        }
 
- return data, nil
+        _ = c.core.Put(setCtx, key, got)
+    }
+
+    return data, nil
 }
 
