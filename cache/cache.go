8cdd053cacd531910cbccfd5fba9f691f522b906
rusfort <rusfort-fax@yandex.ru>
2023-04-26 13:00:24 +0300

    add go.mod

--- cache/cache.go
+++ cache/cache.go
@@ -1,10 +1,10 @@
 package cache
 
 import (
+	"context"
+	"encoding/json"
 	"fmt"
 	"time"
-    "context"
-    "encoding/json"
 )
 
 type ICache interface {
@@ -24,37 +24,36 @@ func NewAbstractCache(core ICache) *AbstractCache {
 type AbstractProc func(ctx context.Context, params ...interface{}) (interface{}, error)
 
 func (c *AbstractCache) GetFromCache(
-    ctx context.Context,
-    keyPrefix string,
-    proc AbstractProc,
-    params ...interface{},
+	ctx context.Context,
+	keyPrefix string,
+	proc AbstractProc,
+	params ...interface{},
 ) ([]byte, error) {
-    var (
-        err  error
-        got  interface{}
-        data []byte
-    )
+	var (
+		err  error
+		got  interface{}
+		data []byte
+	)
 
-    key := keyPrefix
-    for _, p := range params {
-        key += fmt.Sprintf("-%v", p)
-    }
+	key := keyPrefix
+	for _, p := range params {
+		key += fmt.Sprintf("-%v", p)
+	}
 
-    data, err = c.core.Get(ctx, key)
-    if err != nil {
-        got, err = proc(ctx, params...)
-        if err != nil {
-            return nil, fmt.Errorf("error on proc: %w", err)
-        }
-        
-        data, err = json.Marshal(got)
-        if err != nil {
-            return nil, fmt.Errorf("error on marshal: %w", err)
-        }
+	data, err = c.core.Get(ctx, key)
+	if err != nil {
+		got, err = proc(ctx, params...)
+		if err != nil {
+			return nil, fmt.Errorf("error on proc: %w", err)
+		}
 
-        _ = c.core.Put(setCtx, key, got)
-    }
+		data, err = json.Marshal(got)
+		if err != nil {
+			return nil, fmt.Errorf("error on marshal: %w", err)
+		}
 
-    return data, nil
+		_ = c.core.Put(setCtx, key, got)
+	}
+
+	return data, nil
 }
-
